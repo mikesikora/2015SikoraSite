1e73ea8472d64d08301e03f2e3301cca4eaae9a0
mikesikora <msikora.umd@gmail.com>
2015-03-08 23:47:33 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include_once('partials/header.html') ?>
+
+	<!-- Header Style -->
+
+	<header>
+		<div class="container">
+			<div class="brand">
+				<h1 href="#header" class="nav-link">Michael Sikora</h1>
+			</div>
+			<ul class="nav-bar">
+				<li><a href="#about" class="nav-link">About</a></li>
+				<li><a href="#projects" class="nav-link">Projects</a></li>
+				<li><a href="#resume" class="nav-link">Resume</a></li>
+				<li><a href="#contact" class="nav-link">Contact</a></li>
+			</ul>
+		</div>
+	</header>
+
+<?php include_once('partials/footer.html') ?>	
